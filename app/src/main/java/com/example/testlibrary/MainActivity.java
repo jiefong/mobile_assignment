4e4fe987ef6bb50d4e8e5b76ad6082e7e12079f9
SJVoon <brucev90@gmail.com>
2019-11-30 01:02:35 +0800

    LocationInfo Object is created to store location in database

--- app/src/main/java/com/example/testlibrary/MainActivity.java
+++ app/src/main/java/com/example/testlibrary/MainActivity.java
@@ -1,23 +1,37 @@
 package com.example.testlibrary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase database;
     DatabaseReference myRef;
+    FirebaseStorage storage;
+    StorageReference mStorageRef;
+    List<LocationInfo> locationList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +40,20 @@ public class MainActivity extends AppCompatActivity {
 
         //define database & reference
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference("message");
+        myRef = database.getReference().child("LocationList");
+        storage = FirebaseStorage.getInstance();
+        mStorageRef = storage.getReference();
+        locationList = new ArrayList<>();
 
         //put value to database using setValue()
-        myRef.setValue("Hello, World!");
+        //myRef.setValue("Hello, World!");
         //if success means database now has json tree database>message>hello,world!
+        LocationInfo info = new LocationInfo();
+        info.setName("example");
+        info.setX(1500);
+        info.setY(1500);
+        myRef.child("example").setValue(info);
+
 
         // Read from the database using this listener
         myRef.addValueEventListener(new ValueEventListener() {
@@ -38,7 +61,12 @@ public class MainActivity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
+                locationList.clear();
+
+                for(DataSnapshot userSnapShot : dataSnapshot.getChildren()){
+                    LocationInfo u = userSnapShot.getValue(LocationInfo.class);
+                    locationList.add(u);
+                }
             }
 
             @Override
@@ -46,6 +74,27 @@ public class MainActivity extends AppCompatActivity {
                 // Failed to read value
             }
         });
+
+
+        //haven't implement the storage part
+//        Uri file = Uri.fromFile(new File("path/to/images/rivers.jpg"));
+//        StorageReference riversRef = mStorageRef.child("images/rivers.jpg");
+//
+//        riversRef.putFile(file)
+//                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                    @Override
+//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                        // Get a URL to the uploaded content
+//
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception exception) {
+//                        // Handle unsuccessful uploads
+//                        // ...
+//                    }
+//                });
     }
 
     public void goScanActivity(View view){
