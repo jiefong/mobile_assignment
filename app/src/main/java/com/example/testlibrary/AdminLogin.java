97888a43ae7d1eee6d0870199ab0f6d5e030f4be
jiefong <jiefong97@gmail.com>
2019-12-17 23:11:44 +0800

    add adminlandingpage

--- app/src/main/java/com/example/testlibrary/AdminLogin.java
+++ app/src/main/java/com/example/testlibrary/AdminLogin.java
@@ -70,7 +70,7 @@ public class AdminLogin extends AppCompatActivity {
                     Admin.getInstance().setUsername(thisUsername);
                     Admin.getInstance().setPassword(thisPassword);
                     //need change
-                    Intent myIntent = new Intent(AdminLogin.this, AdminMenu.class);
+                    Intent myIntent = new Intent(AdminLogin.this, AdminLandingPage.class);
                     startActivity(myIntent);
                     finish();
                 }
